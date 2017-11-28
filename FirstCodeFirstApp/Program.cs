9f0f600e082bd937bef3ad3600d14841ce281555
zhupa <382211145@qq.com>
2017-11-28 17:53:20 +0800

    添加项目文件。

---
+++ FirstCodeFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

4606193a9aaebecb28245fcd24d350fdd8838c54
zhupa <382211145@qq.com>
2017-12-25 20:54:16 +0800

    使用jwt

---
+++ JwtAuthSample/Models/JwtSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthSample.Models
+{
+    public class JwtSettings
+    {
+        /// <summary>
+        /// 谁颁发的
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// 给谁使用
+        /// </summary>
+        public string Audience { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
---
+++ JwtAuthSample/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+using JwtAuthSample.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtAuthSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //1.添加验证配置
+
+            //在appSettings的配置中要设置好,这个配置需要在第一级中
+            services.Configure<JwtSettings>(Configuration);//注册并注入
+
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind("JwtSettings", jwtSettings); //绑定实例
+
+            //请一定要注意这个和Authorization的区别
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;//不知道是干啥用的
+            })
+            .AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)) //对称加密
+                };
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //2.使用验证
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
